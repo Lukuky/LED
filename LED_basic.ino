1556a22b173baa89dfa66cc959c8602cefda9207
Lukuky <50073534+Lukuky@users.noreply.github.com>
2019-05-02 20:48:42 +0200

    Add files via upload

---
+++ LED_basic.ino
@@ -0,0 +1,17 @@
+/*
+  CONNECT DESCRIPTION
+  anode to some pin (digital or analog)
+  katode to GND
+  for +5V logic you need to add resistor!
+*/
+
+int ledPin = 3; // LED name and relevant pin
+
+void setup() {
+  pinMode(ledPin, OUTPUT); // set led connected with pin and GND
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); // turn on LED
+  //digitalWrite(ledPin, LOW); // turn off LED
+}
---
+++ LED_blink.ino
@@ -0,0 +1,21 @@
+/*
+  CONNECT DESCRIPTION
+  anode to some pin (digital or analog)
+  katode to GND
+  for +5V logic you need to add resistor!
+*/
+
+int ledPin = 3; // LED name and relevant pin
+byte frequency = 1000; //freguency of blinking
+
+void setup() {
+  pinMode(ledPin, OUTPUT); // set led connected with pin and GND
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); // turn on LED
+  delay(frequency); // glowing pause
+  digitalWrite(ledPin, LOW); // turn off LED
+  delay(frequency); // not-glowing pause
+  frequency -= 10;
+}
